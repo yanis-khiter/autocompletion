da5bd241a2f680cb1d47b37c657129173d50df74
yanis-khiter <yanis.khiter@laplateforme.io>
2022-05-04 16:24:24 +0200

    style ok

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Autocompletion</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="fontawesome-free-5.15.1/css/all.css">
+    <script src="https://kit.fontawesome.com/afd6aa68df.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+
+<h1> TIMES ZONE </h1>
+
+    <div class="box">
+
+        <form name="search">
+            <input type="text" class="input" name="txt" placeholder="Entrer votre capitale (UTF) ...">
+        </form>
+
+            <i class="fas fa-search"></i>
+    </div>
+
+    
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,57 @@
+const input = document.querySelector('#fuseau');
+const suggestions = document.querySelector('.suggestions ul');
+
+const fuseau = [ ''];
+
+function search(str) {
+	let results = [];
+	const val = str.toLowerCase();
+
+	for (i = 0; i < fuseau.length; i++) {
+		if (fuseau[i].toLowerCase().indexOf(val) > -1) {
+			results.push(fuseau[i]);
+		}
+	}
+
+	return results;
+}
+
+function searchHandler(e) {
+	const inputVal = e.currentTarget.value;
+	let results = [];
+	if (inputVal.length > 0) {
+		results = search(inputVal);
+	}
+	showSuggestions(results, inputVal);
+}
+
+function showSuggestions(results, inputVal) {
+    
+    suggestions.innerHTML = '';
+
+	if (results.length > 0) {
+		for (i = 0; i < results.length; i++) {
+			let item = results[i];
+			// Highlights only the first match
+			// TODO: highlight all matches
+			const match = item.match(new RegExp(inputVal, 'i'));
+			item = item.replace(match[0], `<strong>${match[0]}</strong>`);
+			suggestions.innerHTML += `<li>${item}</li>`;
+		}
+		suggestions.classList.add('has-suggestions');
+	} else {
+		results = [];
+		suggestions.innerHTML = '';
+		suggestions.classList.remove('has-suggestions');
+	}
+}
+
+function useSuggestion(e) {
+	input.value = e.target.innerText;
+	input.focus();
+	suggestions.innerHTML = '';
+	suggestions.classList.remove('has-suggestions');
+}
+
+input.addEventListener('keyup', searchHandler);
+suggestions.addEventListener('click', useSuggestion);
