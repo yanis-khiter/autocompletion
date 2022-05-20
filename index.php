1f459886c599e8db96e91ef3976709c7e301d512
yanis-khiter <yanis.khiter@laplateforme.io>
2022-05-21 01:35:18 +0200

    85%

--- index.php
+++ index.php
@@ -5,11 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script type="text/javascript" src="style.js"></script>
-    <script type="text/javascript" src="script.js"></script>
+    <script type="text/javascript" src="./css/index.css"></script>
+    <script type="text/javascript" src="./js/style.js"></script>
+    <script type="text/javascript" src="./js/script.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="./css/index.css">
     <title>Autocompletion</title>
 </head>
 <body>
